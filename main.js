7c2e2361396fc50f5a5b2b7dd3b14e9e2d99ce7b
Santi Bascal <sbascalcorrea@gmail.com>
2023-10-12 18:02:03 -0300

    simpleList casi terminado (falto eliminar object del sessionstorage)

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function localSave(texto, itemId) {
         texto,
         itemId
     }
- 
-    
+
+
     if (sessionStorage.getItem('itemSave') === null) {
         itemSave.push(savedItemObj)
         sessionStorage.setItem('itemSave', JSON.stringify(itemSave));
@@ -30,17 +30,16 @@ function localSave(texto, itemId) {
 
 function showSaveItem() {
     const saveItem = JSON.parse(sessionStorage.getItem('itemSave'));
-    console.log(saveItem)
     if (saveItem !== null) {
         for (let x = 0; x < saveItem.length; x += 1) {
             let allItems = saveItem[x];
             let saveText = allItems.texto;
             let saveId = allItems.itemId;
-            
+
             const li = document.createElement('li');
             const itemText = document.createElement('p');
             itemText.textContent = saveText;
-            itemText.setAttribute('class', saveId)
+            li.setAttribute('class', saveId)
 
             li.appendChild(itemText);
             li.appendChild(btnDelete());
@@ -54,13 +53,7 @@ function showSaveItem() {
 
 
 
-function delSaveItem()){
-
-}
-
-
 showSaveItem()
-
 
 
 
@@ -77,7 +70,7 @@ btnAdd.addEventListener('click', (evt) => {
         const itemText = document.createElement('p');
         const itemId = Date.now();
         itemText.textContent = texto;
-        itemText.setAttribute('class', itemId);
+        li.setAttribute('class', itemId);
         li.appendChild(itemText);
         li.appendChild(btnDelete());
         list.appendChild(li);
@@ -96,22 +89,18 @@ btnAdd.addEventListener('click', (evt) => {
 
 
 
-
 function btnDelete() {
     const btnDel = document.createElement('button');
+
 
     btnDel.textContent = 'del';
     btnDel.className = 'btnDel';
 
     btnDel.addEventListener('click', (evt) => {
         const item = evt.target.parentElement;
-
-
         list.removeChild(item);
 
-        const items = document.querySelectorAll('li');
-
-
+        const items = document.querySelectorAll('li')
         if (items.length === 0) {
             noText.style.display = 'block';
             tutorial.style.display = 'block';
